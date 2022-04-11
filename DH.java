1d007c5bca9ab126f9b05b0686329cab9f9b0877
Aryan Shaikh <45735462+Anti-Reverb@users.noreply.github.com>
2022-04-11 10:08:26 +0530

    Add files via upload

---
+++ DH.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+// This program calculates the Key for two persons
+// using the Diffie-Hellman Key exchange algorithm
+class DH{
+     
+    // Power function to return value of a ^ b mod P
+    private static long power(long a, long b, long p)
+    {
+        if (b == 1)
+            return a;
+        else
+            return (((long)Math.pow(a, b)) % p);
+    }
+     
+    // Driver code
+    public static void main(String[] args)
+    {
+        long P, G, x, a, y, b, ka, kb;
+        Scanner sc = new Scanner(System.in);
+         
+        // Both the persons will be agreed upon the
+        // public keys G and P
+         
+        // A prime number P is taken
+        //P = 23;
+        System.out.println("Enter The value of P:");
+        P = sc.nextLong();
+         
+        // A primitive root for P, G is taken
+        //G = 9;
+        System.out.println("Enter The value of G:");
+        G = sc.nextLong();
+         
+        // Alice will choose the private key a
+        // a is the chosen private key
+        //a = 4;
+        System.out.println("Enter The private key a for Alice:");
+        a = sc.nextLong();
+         
+        // Gets the generated key
+        x = power(G, a, P);
+         
+        // Bob will choose the private key b
+        // b is the chosen private key   
+        //b = 3;
+        System.out.println("The private key b for Bob:");
+        b = sc.nextLong();
+         
+        // Gets the generated key
+        y = power(G, b, P);
+         
+        // Generating the secret key after the exchange
+        // of keys
+        ka = power(y, a, P); // Secret key for Alice
+        kb = power(x, b, P); // Secret key for Bob
+         
+        System.out.println("Secret key for the Alice is:" + ka);
+        System.out.println("Secret key for the Bob is:" + kb);
+    }
+    }
