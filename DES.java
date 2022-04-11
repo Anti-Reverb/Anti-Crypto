2277854af51391c4e5fd7bd306ea8fe8b444bb6e
Aryan Shaikh <45735462+Anti-Reverb@users.noreply.github.com>
2022-04-11 10:07:38 +0530

    Add files via upload

---
+++ DES.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+import javax.crypto.*;
+
+public class DES
+{
+	Cipher ecipher,dcipher;
+	public static void main(String args[])
+	{
+		System.out.println("Enter any String :");
+		Scanner sc=new Scanner(System.in);
+		String input=sc.nextLine();
+		try
+		{
+			KeyGenerator k=KeyGenerator.getInstance("DES");
+			SecretKey key=k.generateKey();
+			DES en=new DES(key);
+			String ct=en.encrypt(input);
+			String decrypted=en.decrypt(ct);
+			
+			System.out.println("Original string is:"+input);
+			System.out.println("Encrypted string is : "+ct);
+			System.out.println("Decrypted String is : "+decrypted);
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+
+		sc.close();
+
+	}
+	public DES(SecretKey key)
+	{
+		try
+		{
+			ecipher=Cipher.getInstance("DES");
+			ecipher.init(Cipher.ENCRYPT_MODE,key);
+			dcipher=Cipher.getInstance("DES");
+			dcipher.init(Cipher.DECRYPT_MODE,key);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	public String encrypt(String str)
+	{
+		
+		try
+		{
+			byte[] b=str.getBytes();
+			byte[] enc=ecipher.doFinal(b);
+			return new String(enc);
+		
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return null;
+		
+	}
+	public String decrypt(String str)
+	{
+		try
+		{
+			byte[] b=str.getBytes(); 
+			byte[] dec=dcipher.doFinal(b);
+			return new String(dec);
+		
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return null;
+	}
+}
